8319265a5fb8b6456ce198e8e020c0300800d6f1
Gideon Israel Dsouza <gidisrael@gmail.com>
2018-06-20 16:55:33 +0200

    Written minimal app that works with templating :)

---
+++ run_debug.py
@@ -0,0 +1,4 @@
+from sensorapp import app 
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0',port=51000, debug=True)
--- sensorapp/main/controllers.py
+++ sensorapp/main/controllers.py
@@ -1,4 +1,4 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 
 
 main = Blueprint('main', __name__)
@@ -7,3 +7,13 @@ main = Blueprint('main', __name__)
 @main.route('/')
 def index():
     return "Main"
+
+@main.route('books/')
+def display_books():
+    books = {
+        "A": "Gideon D",
+        }
+
+    # passing data to the template
+    return render_template("books.htm", books=books)
+
